6b02be58d1054dc22a7199b1a0fb170b3f873853
Jhonatan Zubieta <jhonatan.zubieta@moove-it.com>
2023-04-18 22:58:01 -0300

    fix: :bug: added typescript types for woo api

--- src/utils/wp-api.ts
+++ src/utils/wp-api.ts
@@ -1,9 +1,9 @@
 import WooCommerceRestApi from "@woocommerce/woocommerce-rest-api";
 
 const wpApi = new WooCommerceRestApi({
-  url: process.env.NEXT_PUBLIC_WORDPRESS_URL,
-  consumerKey: process.env.WC_CONSUMER_KEY,
-  consumerSecret: process.env.WC_CONSUMER_SECRET,
+  url: process.env.NEXT_PUBLIC_WORDPRESS_URL || "",
+  consumerKey: process.env.WC_CONSUMER_KEY || "",
+  consumerSecret: process.env.WC_CONSUMER_SECRET || "",
   version: "wc/v3",
 });
 
